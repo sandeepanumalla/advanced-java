240bd483a128e4934c2f88b3868929a1572bf997
Sandeep <anumalla.sandeep1999@gmail.com>
2023-07-10 16:16:17 +0530

    day 2 learning

---
+++ servlets-sessions/src/main/java/com/example/servletssessions/Servlet2.java
@@ -0,0 +1,28 @@
+package com.example.servletssessions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet Servlet1</title>");
+        out.println("</head>");
+        out.println("<body>");
+        String name = req.getParameter("name");
+        out.println("<h1>Hello, " + name + " welcome back to my website... </h1>");
+        out.println("<h1>Thank you</h1>");
+        out.println("</body>");
+        out.println("<html>");
+    }
+}
